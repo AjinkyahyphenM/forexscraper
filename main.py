df0be8be57fd51898fd4e7f0bde122e7b9603e18
AjinkyahyphenM <96536389+AjinkyahyphenM@users.noreply.github.com>
2023-12-16 01:03:30 +0530

    testing main

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import scraper
 
 app = Flask(__name__)
 
-@app.route('/api/<string:date>', methods = ["GET"])
-def getData(date):
-    data = scraper.scrape(date)
-    return data
+# @app.route('/api/<string:date>', methods = ["GET"])
+# def getData(date):
+#     data = scraper.scrape(date)
+#     return data
+
+@app.route("", methods = ["GET"])
+def home():
+    return "Hello, world"
 
 if __name__ == "__main__":
     app.run()
