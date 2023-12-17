dcd76f18d58cad314073707634251a1d530b1664
AjinkyahyphenM <96536389+AjinkyahyphenM@users.noreply.github.com>
2023-12-17 13:29:23 +0530

    rollback init

--- scraper.py
+++ scraper.py
@@ -37,15 +37,15 @@ def scrape(date):
 		options.add_argument('--remote-debugging-pipe')
 		#options.add_argument("window-size=1280,720")
 			# # options.add_argument('--no-sandbox')
-		options.add_argument('--headless')
+		#options.add_argument('--headless')
 
 			# # options.add_argument("--disable-setuid-sandbox")
 			# # # other options
 			# # options.add_argument("--disable-extensions")
 			# options.add_argument("--disable-gpu")
 			# options.add_argument('--ignore-certificate-errors')
-		cService = webdriver.ChromeService(executable_path= os.getcwd()+"/chromedriver")
-		driver = webdriver.Chrome(service=cService, options=options) 
+		#cService = webdriver.ChromeService(executable_path= os.getcwd()+"/chromedriver")
+		driver = webdriver.Chrome(service = ChromeService(ChromeDriverManager.install()), options=options) 
 		
 		driver.get(url) 
 		driver.implicitly_wait(2)
@@ -74,9 +74,9 @@ def scrape(date):
 		end_date.send_keys(parsed_date)
 
 		driver.implicitly_wait(100)
-		# download_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div/div/div[5]/div[3]/div/div/div/button")
-		# download_button.click()
-		WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div/div/div[5]/div[3]/div/div/div/button"))).click()
+		download_button = driver.find_element(By.ID, "buttonId")
+		download_button.click()
+		#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div/div/div[5]/div[3]/div/div/div/button"))).click()
 
 		time.sleep(5)
 
