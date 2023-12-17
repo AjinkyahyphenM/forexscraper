6ed9db01f65291aa4c3448eaaac536384aeb5430
AjinkyahyphenM <96536389+AjinkyahyphenM@users.noreply.github.com>
2023-12-17 13:34:17 +0530

    Revert to gunicorn

--- scraper.py
+++ scraper.py
@@ -1,4 +1,3 @@
-import random
 import time
 import os
 from selenium import webdriver 
@@ -7,8 +6,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import parser
 #2023-01-21
 
@@ -28,28 +25,14 @@ def scrape(date):
 	returnJSON = {}
 
 	try:
-		#####OLD####
-		
 		options = webdriver.ChromeOptions()
 		prefs = {"download.default_directory" : os.getcwd()+"/downloads"}
 		options.add_experimental_option("prefs",prefs)
-		options.add_argument("--no-sandbox")
-		options.add_argument('--remote-debugging-pipe')
-		#options.add_argument("window-size=1280,720")
-			# # options.add_argument('--no-sandbox')
-		#options.add_argument('--headless')
-
-			# # options.add_argument("--disable-setuid-sandbox")
-			# # # other options
-			# # options.add_argument("--disable-extensions")
-			# options.add_argument("--disable-gpu")
-			# options.add_argument('--ignore-certificate-errors')
-		#cService = webdriver.ChromeService(executable_path= os.getcwd()+"/chromedriver")
-		driver = webdriver.Chrome(service = ChromeService(ChromeDriverManager.install()), options=options) 
+		driver = webdriver.Chrome(service=ChromeService( 
+			ChromeDriverManager().install()), options=options) 
 		
 		driver.get(url) 
 		driver.implicitly_wait(2)
-		####OLD####
 
 		currency_selector = driver.find_element(By.ID, "forexCurrency")
 		select = Select(currency_selector)
@@ -73,10 +56,9 @@ def scrape(date):
 		end_date.send_keys(Keys.LEFT)
 		end_date.send_keys(parsed_date)
 
-		driver.implicitly_wait(100)
+		driver.implicitly_wait(10)
 		download_button = driver.find_element(By.ID, "buttonId")
 		download_button.click()
-		#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div/div/div[5]/div[3]/div/div/div/button"))).click()
 
 		time.sleep(5)
 
@@ -85,9 +67,8 @@ def scrape(date):
 		print(returnJSON)
 		return returnJSON
 
-	except Exception as e:
-		print(e)
-		returnJSON["error"] = "Selenium session error"
+	except:
+		returnJSON["error"] = "Error in scraping the data"
 		returnJSON["success"] = False
 		print(returnJSON)
 		return returnJSON
